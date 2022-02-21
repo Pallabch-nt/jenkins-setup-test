ae48df143984535bcdf85015080bcce3cce91ed0
Pallabch-nt <pallab@netapp.com>
2022-02-21 15:35:42 +0530

    Updated files

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a-b;
+    }
+
+    public static double divide(int a, int b) {
+        if(b!=0) {
+            return a/b;
+        }
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("THIS IS A TEST OF THE CALCULATOR CLASS");
+        System.out.println(add(2,3));
+    }
 }
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class TestCalculator {
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+public class TestCalculator {
+    @Test
+    public void GivenTwoIntegersAddReturnsAddition() {
+        assertEquals(3,Calculator.add(2,2));
+    }
 }
